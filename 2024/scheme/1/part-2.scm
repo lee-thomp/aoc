dd5a7df0e995bae25a6e4c0138ebfbfe0258599c
Lee Thompson <lee.p.thomp@gmail.com>
2024-12-08 19:21:41 +0000

    Solved day 1 part 2
    No optimisation, fast enough to not really notice.

--- 2024/scheme/1/part-2.scm
+++ 2024/scheme/1/part-2.scm
@@ -5,8 +5,24 @@
 (define (solve file)
   (call-with-input-file file
     (λ (port)
-      ;; Solve here
-      _)))
+      (let loop ([c (get-line port)]
+                 [left  '()]
+                 [right '()])
+        (if (eof-object? c)
+	    ;; Map filter over right list for what appears in left, multiply
+	    ;; left element by count in right, then sum up list.
+            (apply +
+		   (map
+		    (λ (l)
+		      (* l (length (filter (λ (r)
+					     (= r l))
+					   right))))
+		    left))
+	    ;; Build up left and right list; /for me/ every line of input file
+            ;; is 5 digits, 3 spaces, 5 digits and a newline.
+            (loop (get-line port)
+                  (cons (string->number (substring/read-only c 0 5)) left)
+                  (cons (string->number (substring/read-only c 8 13)) right)))))))
 
 (define (main args)
   (exit
