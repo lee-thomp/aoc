16115f9a660817b465f8f337b9063ef3408c534c
Lee Thompson <lee.p.thomp@gmail.com>
2024-12-13 17:24:05 +0000

    Solution for day 3 part 1

--- 2024/scheme/3/part-1.scm
+++ 2024/scheme/3/part-1.scm
@@ -1,12 +1,26 @@
 #!/usr/bin/env -S guile -e main -s
 !#
 (use-modules (ice-9 textual-ports)
+	     (ice-9 regex)
 	     (srfi srfi-1))
+
+(define (match->result m)
+  (* (string->number (match:substring m 1))
+     (string->number (match:substring m 2))))
 
 (define (solve file)
   (call-with-input-file file
-    (λ (port)
-      _)))
+    (compose
+     ;; Map helper function over list of match structures, sum up results
+     (λ (matches)
+       (apply + (map match->result matches)))
+     ;; Get all matches
+     (λ (s)
+       (list-matches
+	(make-regexp "mul\\(([0-9]+),([0-9]+)\\)")
+	s))
+     ;; Slurp file into string
+     get-string-all)))
 
 (define (main args)
   (exit
