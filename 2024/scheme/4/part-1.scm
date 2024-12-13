83195a44fd3d544cd4a60e4894004d8a87487e3f
Lee Thompson <lee.p.thomp@gmail.com>
2024-12-13 23:06:36 +0000

    Almost working solution

--- 2024/scheme/4/part-1.scm
+++ 2024/scheme/4/part-1.scm
@@ -1,19 +1,30 @@
 #!/usr/bin/env -S guile -e main -s
 !#
-(use-modules (ice-9 textual-ports)
+(use-modules (ice-9 format)
+	     (ice-9 textual-ports)
 	     (ice-9 regex)
 	     (srfi srfi-1))
 
-(define (solve file)
-  (call-with-input-file file
-    (compose
-     (λ (s)
-       (error "solve me!"))
-     ;; Slurp file into string
-     get-string-all)))
+(define (solve port)
+    (let* ([input (get-string-all port)]
+	   [line-length (1+ (string-index input #\newline 0))]
+	   [→ "XMAS"]
+	   [↘ (format #f "X.{~a}M.{~@*~a}A.{~@*~a}S" (1+ line-length))]
+	   [↓ (format #f "X.{~a}M.{~@*~a}A.{~@*~a}S" line-length)]
+	   [↙ (format #f "X.{~a}M.{~@*~a}A.{~@*~a}S" (1- line-length))]
+	   [← "SAMX"]
+	   [↖ (format #f "S.{~a}A.{~@*~a}M.{~@*~a}X" (1+ line-length))]
+	   [↑ (format #f "S.{~a}A.{~@*~a}M.{~@*~a}X" line-length)]
+	   [↗ (format #f "S.{~a}A.{~@*~a}M.{~@*~a}X" (1- line-length))])
+      (apply +
+	     (map (compose
+		   length
+		   (λ (r) (list-matches r input)))
+		  (list → ↘ ↓ ↙ ← ↖ ↑ ↗)))))
 
 (define (main args)
   (exit
    (format #t "answer: ~s~%"
            ;; `args' always name script first
-           (solve (cadr args)))))
+	   (call-with-input-file (cadr args)
+	     solve))))
