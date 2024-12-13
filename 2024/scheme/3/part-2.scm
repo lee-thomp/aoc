58717e3f67bb83195289e343d5943b9c1e76368c
Lee Thompson <lee.p.thomp@gmail.com>
2024-12-13 20:44:12 +0000

    Solved day 3 part 2, ugly but works

--- 2024/scheme/3/part-2.scm
+++ 2024/scheme/3/part-2.scm
@@ -11,14 +11,45 @@
 (define (solve file)
   (call-with-input-file file
     (compose
+     ;; Finally get results of all muls and sum up
+     (λ (ms)
+       (apply + (map match->result ms)))
+     ;; Unusual algorithm to create a list of only valid mul expression matches
      (λ (s)
-       (let* ([muls   (list-matches "mul\\(([0-9]+),([0-9]+)\\)" s)]
-	      [dos    (list-matches "do\\(\\)" s)]
-	      [don'ts (list-matches "don't\\(\\)" s)])
-	 ;; The idea I've got is to now build up a list of 'valid' muls by
-	 ;; iterating over the list of muls, consing them to some other list if
-	 ;; their index into the string is more than a do and less than a don't.
-	 (error "solve me!")))
+       (let loop ([muls   (list-matches "mul\\(([0-9]+),([0-9]+)\\)" s)]
+		  [dos    (list-matches "do\\(\\)" s)]
+		  [don'ts (list-matches "don't\\(\\)" s)]
+		  [valid-muls '()])
+	 ;; If no more muls are left to select then return the list of valid
+	 ;; muls
+	 (cond [(null? muls) valid-muls]
+	       ;; If the latest mul comes after a don't and a do then shave off
+	       ;; that do and grab the next one
+	       [(< (match:start (car dos))
+		   (match:start (car don'ts))
+		   (match:start (car muls)))
+		(loop (cdr muls) (cdr dos) don'ts valid-muls)]
+	       ;; The the latest mul comes after a don't then a do then select
+	       ;; it and go the the next don't
+	       [(< (match:start (car don'ts))
+		   (match:start (car dos))
+		   (match:start (car muls)))
+		(loop (cdr muls) dos (cdr don'ts) (cons (car muls) valid-muls))]
+	       ;; Mul expression between don't and do, don't add to list
+	       [(< (match:start (car don'ts))
+		   (match:start (car muls))
+		   (match:start (car dos)))
+		(loop (cdr muls) dos don'ts valid-muls)]
+	       ;; If the current mul is between a do and a don't or is before
+	       ;; both then select it
+	       [else
+		(loop (cdr muls) dos don'ts (cons (car muls) valid-muls))])))
+     ;; This adds a load of 'do()'s and 'don't()'s to the end of the input so
+     ;; the above algorithm works.  This is not very elegant
+     (λ (s)
+       (string-append s
+		      (string-concatenate/shared
+		       (make-list 100 "do()don't()"))))
      get-string-all)))
 
 (define (main args)
