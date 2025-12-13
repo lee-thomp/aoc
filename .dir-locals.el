426f6d5f125fc816f296a6eac3064dc5879c06ab
Lee Thompson <lee.p.thomp@gmail.com>
2025-12-13 18:59:19 +0000

    *-c: emacs dir locals to set gnu style

---
+++ .dir-locals.el
@@ -0,0 +1,5 @@
+;;; Directory Local Variables            -*- no-byte-compile: t -*-
+;;; For more information see (info "(emacs) Directory Variables")
+
+((c-mode . ((c-basic-offset . 2)
+            (c-default-style . "gnu"))))
